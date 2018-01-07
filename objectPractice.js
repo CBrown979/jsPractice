28b8269451319852281f84ebeb9b84afad85fa00
Candice Bianca Brown <candicebrown97@gmail.com>
2018-01-07 09:32:50 +0000

    completed using objects for lookups

--- objectPractice.js
+++ objectPractice.js
@@ -35,3 +35,50 @@ console.log(player);
 
 // Instructions
 // Use the playerNumber variable to lookup player 16 in testObj using bracket notation.
+
+//Using Objects for Lookups 
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  
+  var lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank"
+  };
+  result = lookup[val];
+  
+//   switch(val) {
+//     case "alpha": 
+//       result = "Adams";
+//       break;
+//     case "bravo": 
+//       result = "Boston";
+//       break;
+//     case "charlie": 
+//       result = "Chicago";
+//       break;
+//     case "delta": 
+//       result = "Denver";
+//       break;
+//     case "echo": 
+//       result = "Easy";
+//       break;
+//     case "foxtrot": 
+//       result = "Frank";
+//   }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+console.log(phoneticLookup("charlie"));
+console.log(phoneticLookup("bravo"));
+console.log(phoneticLookup("foxtrot"));
+console.log(phoneticLookup("echo"));
