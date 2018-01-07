324b8995eaeb386a427117c57373571b97c1cf1f
Candice Bianca Brown <candicebrown97@gmail.com>
2018-01-07 00:23:06 +0000

    added more practice with Switch Statements

--- switchStatement.js
+++ switchStatement.js
@@ -60,3 +60,109 @@ function goToMeals(time){
     return food;
 }
 console.log(goToMeals("Afternoon"));
+
+
+//Adding a Default Option  and multiples with same answer
+function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val){
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+  
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+switchOfStuff("c");
+
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val){
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+console.log(sequentialSizes(7));
+console.log(sequentialSizes(3));
+
+
+//REPLACING IF/ELSE with SWITCH STATEMENTS
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val){
+      case "bob":
+          answer = "Marley";
+          break;
+      case 42:
+          answer = "The Answer";
+          break;
+      case 1:
+          answer = "There is no #1";
+          break;
+      case 99:
+          answer = "Missed me by this much!";
+          break;
+      case 7:
+          answer = "Ate Nine";
+          break;
+  }
+  
+//   if (val === "bob") {
+//     answer = "Marley";
+//   } else if (val === 42) {
+//     answer = "The Answer";
+//   } else if (val === 1) {
+//     answer = "There is no #1";
+//   } else if (val === 99) {
+//     answer = "Missed me by this much!";
+//   } else if (val === 7) {
+//     answer = "Ate Nine";
+//   }
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+console.log(chainToSwitch("bob"));
+console.log(chainToSwitch(99));
+console.log(chainToSwitch(7));
+
+
+
+
