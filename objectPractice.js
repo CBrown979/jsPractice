af3aa0e715c63c55e431bb5b7d5c6d0cbbc0cc67
Candice Bianca Brown <candicebrown97@gmail.com>
2018-01-07 10:22:16 +0000

    accessing nested objects

--- objectPractice.js
+++ objectPractice.js
@@ -145,3 +145,43 @@ console.log(checkObj("pet"));
 // myObj.hasOwnProperty("middle"); // false
 // Instructions
 // Modify the function checkObj to test myObj for checkProp. If the property is found, return that property's value. If not, return "Not Found".
+
+//Accessing Nested Objects 
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+// Only change code below this line
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+console.log(gloveBoxContents);
+
+// The sub-properties of objects can be accessed by chaining together the dot or bracket notation.
+
+// Here is a nested object:
+
+// var ourStorage = {
+//   "desk": {
+//     "drawer": "stapler"
+//   },
+//   "cabinet": {
+//     "top drawer": { 
+//       "folder1": "a file",
+//       "folder2": "secrets"
+//     },
+//     "bottom drawer": "soda"
+//   }
+// };
+// ourStorage.cabinet["top drawer"].folder2;  // "secrets"
+// ourStorage.desk.drawer; // "stapler"
+// Instructions
+// Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use bracket notation for properties with a space in their name.
